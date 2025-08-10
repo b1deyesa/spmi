59421124987eda0a4e27118eadf0c2d6b19db4e0
= <=>
2025-08-10 17:50:25 +0800

    First commit

--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -21,7 +21,7 @@
                 </div>
             </div>
         </div>
-        {{-- <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2">
             <div class="flex items-center justify-between text-sm p-2 pl-4 pr-3 rounded-md border border-zinc-300 bg-zinc-50">
                 <div class="flex flex-col">
                     <p>Monitoring Skor Toefl Lulusan UNPATTI</p>
@@ -108,6 +108,6 @@
                     <a href="{{ route_f('form.rekapitulasi-evaluasi-skor-toefl-lulusan', ['programStudi' => request()->route('fakultas')->programStudis()->orderBy('nama', 'asc')?->first()?->id, 'year' => date('Y')]) }}" class="flex items-center justify-center px-3 py-1 rounded-sm bg-gray-600 text-zinc-50">Buka File</a>
                 </div>
             </div>
-        </div> --}}
+        </div>
     </div>
 </x-layout.form>
