c45f23a23521987a59752c3810e52c15a2910252
= <=>
2025-08-24 22:22:33 +0700

    Auto Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,54 +17,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            AkreditasiSeeder::class,
-            JenjangSeeder::class,
-            FakultasSeeder::class,
-            ProgramStudiSeeder::class,
-            KebijakanSeeder::class,
-            RoleSeeder::class,
-            ProfilSeeder::class,
-            PenetapanSeeder::class,
-        ]);
+        User::find(8)->fakultases()->sync([1,2,3,4,5,6,7,8,9,10,11]);
         
-        // // Fakultas::factory(10)->create();
-        // // ProgramStudi::factory(20)->create();
-        // // Dosen::factory(100)->create();
-        // // Mahasiswa::factory(300)->create();
+        // $this->call([
+        //     AkreditasiSeeder::class,
+        //     JenjangSeeder::class,
+        //     FakultasSeeder::class,
+        //     ProgramStudiSeeder::class,
+        //     KebijakanSeeder::class,
+        //     RoleSeeder::class,
+        //     ProfilSeeder::class,
+        //     PenetapanSeeder::class,
+        // ]);
         
-        $deo = User::factory()->create([
-            'role_id' => 1,
-            'name' => 'Imagodeo Bideyesa',
-            'email' => 'bideyesa@gmail.com',
-            'password' => Hash::make('magox1905'),
-        ]);
-        $deo->fakultases()->sync([5,7,10]);
+        // // // Fakultas::factory(10)->create();
+        // // // ProgramStudi::factory(20)->create();
+        // // // Dosen::factory(100)->create();
+        // // // Mahasiswa::factory(300)->create();
         
-        $users = [
-            ['name' => 'Paul', 'email' => 'paul@example.com', 'role_id' => 1, 'fakultas_id' => [11]],
-            ['name' => 'Johan', 'email' => 'johan@example.com', 'role_id' => 1, 'fakultas_id' => [1]],
-            ['name' => 'Arman', 'email' => 'arman@example.com', 'role_id' => 1, 'fakultas_id' => [3]],
-            ['name' => 'Lisye', 'email' => 'lisye@example.com', 'role_id' => 1, 'fakultas_id' => [5]],
-            ['name' => 'Nova', 'email' => 'nova@example.com', 'role_id' => 1, 'fakultas_id' => [7]],
-            ['name' => 'Jufri', 'email' => 'jufri@example.com', 'role_id' => 2, 'fakultas_id' => [1]],
-            ['name' => 'James', 'email' => 'james@example.com', 'role_id' => 2, 'fakultas_id' => [1]],
-            ['name' => 'Bruri', 'email' => 'bruri@example.com', 'role_id' => 1, 'fakultas_id' => [8]],
-            ['name' => 'FKIP', 'email' => 'fkip@esipenjamu.com', 'role_id' => 1, 'fakultas_id' => [5]],
-            ['name' => 'FKIP', 'email' => 'asesor@esipenjamu.com', 'role_id' => 1, 'fakultas_id' => [1,2]],
-        ];
+        // $deo = User::factory()->create([
+        //     'role_id' => 1,
+        //     'name' => 'Imagodeo Bideyesa',
+        //     'email' => 'bideyesa@gmail.com',
+        //     'password' => Hash::make('magox1905'),
+        // ]);
+        // $deo->fakultases()->sync([5,7,10]);
+        
+        // $users = [
+        //     ['name' => 'Paul', 'email' => 'paul@example.com', 'role_id' => 1, 'fakultas_id' => [11]],
+        //     ['name' => 'Johan', 'email' => 'johan@example.com', 'role_id' => 1, 'fakultas_id' => [1]],
+        //     ['name' => 'Arman', 'email' => 'arman@example.com', 'role_id' => 1, 'fakultas_id' => [3]],
+        //     ['name' => 'Lisye', 'email' => 'lisye@example.com', 'role_id' => 1, 'fakultas_id' => [5]],
+        //     ['name' => 'Nova', 'email' => 'nova@example.com', 'role_id' => 1, 'fakultas_id' => [7]],
+        //     ['name' => 'Jufri', 'email' => 'jufri@example.com', 'role_id' => 2, 'fakultas_id' => [1]],
+        //     ['name' => 'James', 'email' => 'james@example.com', 'role_id' => 2, 'fakultas_id' => [1]],
+        //     ['name' => 'Bruri', 'email' => 'bruri@example.com', 'role_id' => 1, 'fakultas_id' => [8]],
+        //     ['name' => 'FKIP', 'email' => 'fkip@esipenjamu.com', 'role_id' => 1, 'fakultas_id' => [5]],
+        //     ['name' => 'FKIP', 'email' => 'asesor@esipenjamu.com', 'role_id' => 1, 'fakultas_id' => [1,2]],
+        // ];
 
-        foreach ($users as $data) {
-            $user = User::factory()->create([
-                'role_id' => $data['role_id'],
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => Hash::make('sipenjamu-demo'),
-            ]);
+        // foreach ($users as $data) {
+        //     $user = User::factory()->create([
+        //         'role_id' => $data['role_id'],
+        //         'name' => $data['name'],
+        //         'email' => $data['email'],
+        //         'password' => Hash::make('sipenjamu-demo'),
+        //     ]);
 
-            if ($data['fakultas_id']) {
-                $user->fakultases()->sync($data['fakultas_id']);
-            }
-        }
+        //     if ($data['fakultas_id']) {
+        //         $user->fakultases()->sync($data['fakultas_id']);
+        //     }
+        // }
     }
 }
